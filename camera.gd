1fac7ac9035606610bb0c312ab03b903b801a1f0
Leo Peltola <leopeltola@gmail.com>
2023-03-05 18:16:20 +0200

    Switched to normal buffer, added negative depth to prevent highlight leaks

--- camera.gd
+++ camera.gd
@@ -7,6 +7,9 @@ extends Camera3D
 		if p:
 			$Postprocess.show()
 			post_processing = p
+			var a = Vector3(-1, 1, 0).normalized()
+			var b = Vector3(1, 0, 0).normalized()
+			print("dot: ", a.dot(b))
 		else:
 			$Postprocess.hide()
 			post_processing = p
